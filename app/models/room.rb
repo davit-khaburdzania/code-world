ee1ec6d2d809aa732169e92dab58b042f92d465d
gchkhvirkia <Georgechkhvirkia@gmail.com>
2014-12-07 00:17:33 +0300

    created experience migr. and model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ActiveRecord::Base
   belongs_to :house
   belongs_to :user
   has_one :quiz
+
+  has_many :experiences
+  has_many :skills, :through => :experiences
+
 end
---
+++ db/migrate/20141206210202_create_experiences.rb
@@ -0,0 +1,11 @@
+class CreateExperiences < ActiveRecord::Migration
+  def change
+    create_table :experiences do |t|
+      t.belongs_to :skill
+      t.belongs_to :room
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
