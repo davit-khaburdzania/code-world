8b35cd343a762f95eb7ea9a03e5bad966369e2bb
gchkhvirkia <Georgechkhvirkia@gmail.com>
2014-12-06 15:02:17 +0300

    updated dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,16 @@ ActiveRecord::Schema.define(version: 20141206101938) do
   create_table "users", force: true do |t|
     t.string   "full_name"
     t.integer  "badge_id"
-    t.integer  "points"
-    t.float    "health"
+    t.integer  "points",                 default: 100
+    t.float    "health",                 default: 100.0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
