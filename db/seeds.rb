060f04dffd20cad2162ee2bc59d80641f583fe19
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-12-07 00:01:05 +0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#generate user
+user = User.create full_name: "Jon Doe", email: "user@mail.com", password: "qwerasdf"
+
+#generate worlds
+ruby_world = World.create name: "Ruby"
+js_world = World.create name: "Javascript"
+
+#generate houses
+loop_house = House.new place_id: 1, title: 'Loops'
+loop_house.user = user
+ruby_world.houses << loop_house
+ruby_world.save
+
+loop_house = House.new place_id: 3, title: 'Hashes'
+loop_house.user = user
+ruby_world.houses << loop_house
+ruby_world.save
+
+loop_house = House.new place_id: 4, title: 'IO'
+loop_house.user = user
+ruby_world.houses << loop_house
+ruby_world.save
+
+
+
